8e6f591f9f5120b0bc5c7f9ade3e0594f02e7475
lourvens <lourvens.luxamar@gmail.com>
2023-09-23 11:34:16 -0400

    create and add style to the linkinfo modal components

--- client/src/pages/home/ShortenedLinkInfoModal/index.tsx
+++ client/src/pages/home/ShortenedLinkInfoModal/index.tsx
@@ -1,5 +1,15 @@
-import { Modal, ModalBody, ModalContent, Spacer } from "@nextui-org/react";
+import {
+  Button,
+  Divider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+} from "@nextui-org/react";
 import { useLinkModal } from "../../../context/LinkModalContext";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import { AiFillCopy } from "react-icons/ai";
+import { IoAnalyticsOutline } from "react-icons/io5";
 
 const ShortenedLinkInfoModal = () => {
   const { isOpen, onClose, props } = useLinkModal();
@@ -7,17 +17,36 @@ const ShortenedLinkInfoModal = () => {
   return (
     <Modal
       isOpen={isOpen}
+      placement="center"
       size="md"
+      backdrop="blur"
+      classNames={{
+        backdrop: "bg-gray-900 bg-opacity-80",
+        wrapper: "bg-blur backdrop-blur",
+      }}
       onOpenChange={(isOpen) => {
         !isOpen && onClose();
       }}
     >
       <ModalContent>
+        <ModalHeader className="flex items-center gap-3">
+          <Button isIconOnly size="sm" radius="full" variant="shadow">
+            <BsFillPatchCheckFill size="24" />
+          </Button>
+          Url shortened !
+        </ModalHeader>
         <ModalBody>
-          here's your shorten url !
-          <Spacer y={2} />
-          {props?.original_url} and
-          {props?.short_url}
+          <span className="italic">Here's your short link:</span>
+          <div className="flex items-center justify-between bg-white bg-opacity-5 rounded-xl pl-3">
+            <h3 className="text-primary-500">{props?.short_url}</h3>
+            <Button startContent={<AiFillCopy />} color="primary">
+              Copy Link
+            </Button>
+          </div>
+          <Divider />
+          <Button color="warning" startContent={<IoAnalyticsOutline />}>
+            Get Analytics Data
+          </Button>
         </ModalBody>
       </ModalContent>
     </Modal>
